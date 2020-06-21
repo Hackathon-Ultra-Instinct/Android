24e2c091408d101c58bb3ea5d9a3c1900490a2df
Dhruv Jain <dhruvjain2212000@gmail.com>
2020-06-21 11:22:14 +0530

    Boarding activity and stats display working

---
+++ VISApay/app/src/main/java/com/example/android/visapay/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.example.android.visapay;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+import android.view.animation.AccelerateDecelerateInterpolator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.eftimoff.androipathview.PathView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        final PathView pathView = (PathView) findViewById(R.id.pathView);
+        pathView.getPathAnimator()
+                .delay(500)
+                .duration(1000)
+                .interpolator(new AccelerateDecelerateInterpolator())
+                .start();
+
+        pathView.useNaturalColors();
+        pathView.setFillAfter(true);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+//                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+//                finish();
+            }
+        }, 3000);
+
+    }
+}
